61c6288d8829a4e0aa9713763f0594c59dbb1dec
engkuazrul <engkuazrull@gmail.com>
2021-05-23 15:58:20 +0800

    changing root response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send(database.users)});
+app.get('/', (req, res) => {res.send('it is workings')});
 
 app.post('/signin', signin.handleSignin(db, bcrypt)); // alternative formatting function
 
