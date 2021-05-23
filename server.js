23be44b0871bd099c317a424a1f6e130c2cbe0db
engkuazrul <engkuazrull@gmail.com>
2021-05-23 15:46:22 +0800

    adding port

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/', (req, res) => {res.send(database.users)});
+
 app.post('/signin', signin.handleSignin(db, bcrypt)); // alternative formatting function
 
 app.post('/register',(req, res) => {register.handleRegister(req, res, db, bcrypt)});
@@ -33,7 +35,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
 
